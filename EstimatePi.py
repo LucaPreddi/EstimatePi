f527f25911ee07d1144f203272f24eefc2bfdfc2
Luca Predieri <85370395+LucaPreddi@users.noreply.github.com>
2021-11-12 17:40:14 +0100

    Add files via upload

---
+++ EstimatePi.py
@@ -0,0 +1,24 @@
+import random
+
+
+def estimate_pi(n):
+    num_point_circle = 0
+    num_point_total = 0
+    for _ in range(n):
+        x = random.uniform(0, 1)
+        y = random.uniform(0, 1)
+        distance = x ** 2 + y ** 2
+        if distance <= 1:
+            num_point_circle += 1
+        num_point_total += 1
+
+    return 4 * num_point_circle / num_point_total
+
+
+def main():
+    print("Write how many dots you want in your square-circle!\n")
+    x = input()
+    print("Your pi is: ", estimate_pi(int(x)))
+
+
+main()
